8dc2ce4b0a7db883f410f3af83ce2fb14d75e0d2
erikcas <erikcas1972@gmail.com>
2013-03-09 22:49:52 +0100

    extras

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,3 +38,5 @@ TARGET_RECOVERY_FSTAB := device/samsung/beni/recovery/recovery.fstab
 SMALLER_FONT_FOOTPRINT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/beni/bluetooth
 
+## OTA script extras file (build/tools/releasetools)
+TARGET_OTA_EXTRAS_FILE := device/samsung/beni/releasetools-extras.txt
